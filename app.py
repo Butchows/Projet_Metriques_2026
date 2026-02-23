bd00478132ea5efac7a80e6a6b98fbc50953e0d3
Butchows <128541616+Butchows@users.noreply.github.com>
2026-02-23 14:56:20 +0000

    ajout de la metrique finale personnalisee

--- app.py
+++ app.py
@@ -38,6 +38,29 @@ def mongraphique():
 def histogramme():
     return render_template("graphique2.html")
 
+@app.get("/lisbonne")
+def api_lisbonne():
+    
+    url = "https://api.open-meteo.com/v1/forecast?latitude=38.7167&longitude=-9.1333&hourly=temperature_2m,rain,wind_speed_50m&models=meteofrance_seamless"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+    rain = data.get("hourly", {}).get("rain", [])
+    wind_speed = data.get("hourly", {}).get("wind_speed_50m", [])
+
+    n = min(len(times), len(temps), len(rain), len(wind_speed))
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i], "pluie_mm": rain[i], "vitesse_du_vent_km_h": wind_speed[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/atelier")
+def atelier():
+    return render_template("atelier.html")
 # Ne rien mettre après ce commentaire
     
 if __name__ == "__main__":
